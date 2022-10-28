629f2efc6088df3bb3e537da5e844dccf4478d05
a1Th <89348236+a1Thiago@users.noreply.github.com>
2022-10-28 07:40:47 -0300

    webpack?

--- next.config.js
+++ next.config.js
@@ -2,6 +2,17 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+
+  future: {
+    webpack5: true,
+  },
+
+  // webpack: function (config) {
+  //   config.plugins.push(new webpack.EnvironmentPlugin({ ...process.env }))
+  //   return config;
+  // },
+
 }
 
 module.exports = nextConfig
+
